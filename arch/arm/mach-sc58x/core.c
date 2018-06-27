17ea8884e3666df21af6229d1f603bf8225ecf62
jian chen <jian.chen@analog.com>
2018-06-27 01:33:37 -0400

    [LINUX-1516] Enable Ethernet for sc58x boards
    - Add Ethernet support for sc584-ezkit board
    - Add Ethernet support for sc589-ezkit 2.0 board (with DP83867 by default)
    - Add Ethernet support for sc589-mini board (with DP83867 by default)

--- arch/arm/mach-sc58x/core.c
+++ arch/arm/mach-sc58x/core.c
@@ -135,6 +135,50 @@ static struct of_device_id sc58x_of_bus_ids[] __initdata = {
 };
 #endif
 
+#define DP83865_PHY_ID          0x20005c7a
+#define REG_DP83865_AUX_CTRL    0x12
+#define BITP_AUX_CTRL_RGMII_EN  12
+#define RGMII_3COM_MODE         3
+static int sc58x_dp83865_fixup(struct phy_device *phydev)
+{
+	int  phy_data = 0;
+
+	phy_data = phy_read(phydev, REG_DP83865_AUX_CTRL);
+
+	/* enable 3com mode for RGMII */
+	phy_write(phydev, REG_DP83865_AUX_CTRL,
+			     (RGMII_3COM_MODE << BITP_AUX_CTRL_RGMII_EN) | phy_data);
+
+	return 0;
+}
+
+#define DP83848_PHY_ID          0x20005c90
+#define REG_DP83848_PHY_MICR    0x11
+#define BITM_PHY_MICR_INTEN     0x2
+#define BITM_PHY_MICR_INT_OE    0x1
+static int sc58x_dp83848_fixup(struct phy_device *phydev)
+{
+	phy_write(phydev, REG_DP83848_PHY_MICR,
+				BITM_PHY_MICR_INTEN | BITM_PHY_MICR_INT_OE);
+
+	return 0;
+}
+
+static void sc58x_init_ethernet(void)
+{
+	if (IS_BUILTIN(CONFIG_PHYLIB)) {
+		/* select RGMII as the external PHY interface for EMAC0 */
+		writel((readl(__io_address(REG_PADS0_PCFG0)) |
+		        BITM_PADS_PCFG0_EMACPHYISEL | BITM_PADS_PCFG0_EMACRESET),
+		        __io_address(REG_PADS0_PCFG0));
+		/* register fixup to be run for PHYs */
+		phy_register_fixup_for_uid(DP83865_PHY_ID, 0xffffffff,
+				sc58x_dp83865_fixup);
+		phy_register_fixup_for_uid(DP83848_PHY_ID, 0xffffffff,
+				sc58x_dp83848_fixup);
+	}
+}
+
 void __init sc58x_init(void)
 {
 #ifdef CONFIG_CACHE_L2X0
@@ -149,6 +193,7 @@ void __init sc58x_init(void)
 	of_platform_populate(NULL, sc58x_of_bus_ids,
 				sc58x_auxdata_lookup, NULL);
 #endif
+	sc58x_init_ethernet();
 }
 
 static void __iomem *spu_base;
