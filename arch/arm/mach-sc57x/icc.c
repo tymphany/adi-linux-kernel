908e66af50f5d9342e16d8f6a4af490e6db29146
Huanhuan Feng <huanhuan.feng@analog.com>
2018-07-26 08:35:49 -0400

    [LINUX-1484] Bring up icc driver for sc5xx for kernel v4.16 - Bring up icc driver for sc589-ezkit for kernel v4.16 - Bring up icc driver for sc584-ezkit for kernel v4.16 - Bring up icc driver for sc573-ezkit for kernel v4.16
    [LINUX-1562] Bring up core control driver for sc5xx in kernel v4.16
    - Bring up core control driver for sc589-ezkit board in kernel verison 4.16
    - Bring up core control driver for sc584-ezkit board in kernel verison 4.16
    - Bring up core control driver for sc573-ezkit board in kernel verison 4.16
    
    Signed-off-by: Huanhuan Feng <huanhuan.feng@analog.com>

---
+++ arch/arm/mach-sc57x/icc.c
@@ -0,0 +1,50 @@
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/interrupt.h>
+#include <linux/delay.h>
+#include <linux/irq.h>
+#include <linux/gpio.h>
+#include <mach/hardware.h>
+#include <mach/sc57x.h>
+#include <asm/io.h>
+
+void platform_send_ipi_cpu(unsigned int cpu, int irq)
+{
+	BUG_ON(cpu > 2);
+	writel(cpu + TRGM_SOFT0, __io_address(REG_TRU0_MTR));
+}
+
+void platform_send_ipi(cpumask_t callmap, int irq)
+{
+	unsigned int cpu;
+
+	for_each_cpu(cpu, callmap) {
+		platform_send_ipi_cpu(cpu, irq);
+	}
+}
+
+void platform_clear_ipi(unsigned int cpu, int irq)
+{
+}
+
+static irqreturn_t coreb_resource_manage_dummy(int irq, void *dev_id)
+{
+	return 1;
+}
+
+int platform_res_manage_request_irq(uint16_t subid, unsigned int cpu)
+{
+	return 0;
+}
+
+void platform_res_manage_free_irq(uint16_t subid)
+{
+}
+
+void platform_ipi_init(void)
+{
+	writel(TRGM_SOFT0, __io_address(REG_TRU0_SSR71));
+	writel(TRGM_SOFT1, __io_address(REG_TRU0_SSR75));
+	writel(TRGM_SOFT2, __io_address(REG_TRU0_SSR79));
+	writel(1, __io_address(REG_TRU0_GCTL));
+}
