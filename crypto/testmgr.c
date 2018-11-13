ebc9388b833cc6d92825fe6049c6d864abae2fd6
jian chen <jian.chen@analog.com>
2018-11-12 21:05:31 -0500

    add ADI CRC driver support for SC5XX
    enable ADI CRC driver for sc5xx boards in Linux kernel
    
    Signed-off-by: jian chen <jian.chen@analog.com>

--- crypto/testmgr.c
+++ crypto/testmgr.c
@@ -4583,7 +4583,7 @@ static const struct alg_test_desc alg_test_descs[] = {
 		.test = alg_test_hash,
 		.fips_allowed = 1,
 		.suite = {
-			.hash = __VECS(ghash_tv_template)
+			.hash = __VECS(adi_crc_tv_template)
 		}
 	}, {
 		.alg = "hmac(md5)",
