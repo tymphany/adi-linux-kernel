982d0871d072d06a8edd0b3ce81045962b5bef9c
sullivan.liu <sullivan.liu@analog.com>
2019-08-27 01:06:44 -0400

    [LINUX-1628] MCAPI communication between arm core and sharc core2 on sc5xx
    	- Fix an issue that the Core2 can't get Core0's endpoints and intercommunicate
            - Reallocate the shared L2 memory for Core0 and Core2 following the BareMetal MCAPI BSP lib
            - Enable to use the command "corecontrol --start CORE_NUM" to zero the corresponding ICC region
            - Move the file core_control.c from "arch/arm/mach-sc5xx/" to "driver/staging/icc/core/"
            - Enable the core_control driver to get the icc L2 shared memory address from sc5xx.dts
    
    Signed-off-by: sullivan.liu <sullivan.liu@analog.com>

--- arch/arm/mach-sc57x/icc.c
+++ arch/arm/mach-sc57x/icc.c
@@ -6,6 +6,7 @@
 #include <linux/gpio.h>
 #include <mach/hardware.h>
 #include <mach/sc57x.h>
+#include <mach/icc.h>
 #include <asm/io.h>
 
 void platform_send_ipi_cpu(unsigned int cpu, int irq)
--- arch/arm/mach-sc57x/include/mach/icc.h
+++ arch/arm/mach-sc57x/include/mach/icc.h
@@ -18,17 +18,42 @@
 #define invalidate_dcache_range(start, end) __sync_cache_range_r((void *)start, end - start)
 #define flush_dcache_range(start, end) __sync_cache_range_w((void *)start, end - start)
 #define arm_core_id()	0
-#define CCTRL_CORE1     1
-#define CCTRL_CORE2     2
 
-#define CMD_CORE_START              _IO('b', 0)
-#define CMD_CORE_STOP               _IO('b', 1)
-#define CMD_SET_SVECT1              _IO('m', 17)
-#define CMD_SET_SVECT2              _IO('m', 18)
+/* RCU0 (reset core unit) register structure */
+struct rcu_reg {
+	/* RCU0 Control Register */
+	uint32_t reg_rcu_ctl;							/* 0x00 */
+	/* RCU0 Status Register */
+	uint32_t reg_rcu_stat;							/* 0x04 */
+	/* RCU0 Core Reset Control Register */
+	uint32_t reg_rcu_crctl;							/* 0x08 */
+	/* RCU0 Core Reset Status Register */
+	uint32_t reg_rcu_crstat;						/* 0x0c */
+	/* reg pad from 0x10 to 0x1b */
+	uint8_t	 pad_0x10_0x1b[0x1c - 0x10];			/* 0x10 ~ 0x1b */
+	/* RCU0 System Interface Disable Register */
+	uint32_t reg_rcu_sidis;							/* 0x1c */
+	/* RCU0 System Interface Status Register */
+	uint32_t reg_rcu_sistat;						/* 0x20 */
+	/* reg pad from 0x24 to 0x27 */
+	uint8_t	 pad_0x24_0x27[0x28 - 0x24];			/* 0x24 ~ 0x27 */
+	/* RCU0 Boot Code Register */
+	uint32_t reg_rcu_bcode;							/* 0x28 */
+	/* Software Vector Register 0 to 2 */
+	uint32_t reg_rcu_svect0;						/* 0x2c */
+	uint32_t reg_rcu_svect1;						/* 0x30 */
+	uint32_t reg_rcu_svect2;						/* 0x34 */
+	/* reg pad from 0x38 to 0x6f */
+	uint8_t	 pad_0x38_0x6f[0x70 - 0x38];			/* 0x38 ~ 0x6f */
+	/* RCU0 Message Set Bits Register */
+	uint32_t reg_rcu_msg_set;						/* 0x70 */
+};
 
-void platform_send_ipi(cpumask_t callmap, int irq);
 void platform_send_ipi_cpu(unsigned int cpu, int irq);
+void platform_send_ipi(cpumask_t callmap, int irq);
 void platform_clear_ipi(unsigned int cpu, int irq);
 int platform_res_manage_request_irq(uint16_t subid, unsigned int cpu);
+void platform_res_manage_free_irq(uint16_t subid);
+void platform_ipi_init(void);
 
 #endif
