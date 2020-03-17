66124500279c87e2a5d20020469313d3a7245f79
huanhuan feng <huanhuan.feng@analog.com>
2020-03-17 14:26:43 +0800

    [LNXDSP-295] Update ADI sound card to use componenent driver
    Signed-off-by: huanhuan feng <huanhuan.feng@analog.com>

--- crypto/testmgr.c
+++ crypto/testmgr.c
@@ -3102,7 +3102,12 @@ static const struct alg_test_desc alg_test_descs[] = {
 		.fips_allowed = 1,
 		.suite = {
 			.hash = __VECS(ghash_tv_template)
-			.hash = __VECS(bfin_crc_tv_template)
+		}
+	}, {
+		.alg = "hmac(crc32)",
+		.test = alg_test_hash,
+		.suite = {
+			.hash = __VECS(adi_crc_tv_template)
 		}
 	}, {
 		.alg = "hmac(md5)",
