2ba6f20a2e0b9f7daf25325da639aec8a980dee1
huanhuan feng <huanhuan.feng@analog.com>
2020-05-18 08:24:23 +0000

    [LNXDSP-210] Add support to compile with GCC8 rather than CCES toolchain
    Signed-off-by: huanhuan feng <huanhuan.feng@analog.com>

--- drivers/mmc/host/dw_mmc-pltfm.h
+++ drivers/mmc/host/dw_mmc-pltfm.h
@@ -12,6 +12,8 @@
 #ifndef _DW_MMC_PLTFM_H_
 #define _DW_MMC_PLTFM_H_
 
+#include <dw_mmc.h>
+
 extern int dw_mci_pltfm_register(struct platform_device *pdev,
 				const struct dw_mci_drv_data *drv_data);
 extern int dw_mci_pltfm_remove(struct platform_device *pdev);
