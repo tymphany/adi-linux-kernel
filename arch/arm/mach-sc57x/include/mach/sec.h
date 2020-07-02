7fb0c04e5472c1635e7e9d33d38078b5a1b7d7f7
huanhuan feng <huanhuan.feng@analog.com>
2020-07-02 05:55:57 -0400

    [LNXDSP-270] Add remoteproc feature for sc5xx boards
    - Implement a load function to load the ADI LDR file to memory, the LDR file
    is the SHARC application and generated from CCES
    - The start function would boot the SHARC core and execute the SHARC application
    - The stop function would raise a SOFT IRQ to SHARC core and SHARC would release
    all resources and set itself to IDLE
    
    Signed-off-by: huanhuan feng <huanhuan.feng@analog.com>

--- arch/arm/mach-sc57x/include/mach/sec.h
+++ arch/arm/mach-sc57x/include/mach/sec.h
@@ -8,8 +8,8 @@
 
 void sec_init(void __iomem *, void __iomem *, void __iomem *);
 void sec_raise_irq(unsigned int irq);
-void sec_enable_sci(unsigned int sid, bool fault);
-void sec_enable_ssi(unsigned int sid);
+void sec_enable_sci(unsigned int coreid);
+void sec_enable_ssi(unsigned int sid, bool fault, bool source);
 void sec_set_ssi_coreid(unsigned int sid, unsigned int coreid);
 
 #endif /* __MACH_SEC_H */
